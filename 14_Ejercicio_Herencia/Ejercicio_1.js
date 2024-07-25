4211b82d4ff1c8524a9cbe0fece62882514891de
melqui <elmenk2018@gmail.com>
2024-07-25 17:59:26 -0600

    Actualizado

--- 14_Ejercicio_Herencia/Ejercicio_1.js
+++ 14_Ejercicio_Herencia/Ejercicio_1.js
@@ -38,23 +38,23 @@ class Persona{
     
     //toString
     toString(){
-        return this._idPersonas+' '+this._nombre+' '+this._apellido+' '+this._edad;
+        return this._nombre+' '+this._apellido+' '+this._edad;
     }
 
 }
 
 //clase empleado
 class Empleado extends Persona{
-    //static contadorEmpleados = 0;
+    static contadorEmpleados = 0;
     constructor(nombre, apellido, edad, sueldo){
         super(nombre, apellido, edad)
-        //this._idEmpleado = ++Empleado.contadorEmpleados;
+        this._idEmpleado = ++Empleado.contadorEmpleados;
         this._sueldo = sueldo;
     }
 
-    // get idEmpleado(){
-    //     return this._idEmpleado;
-    // }
+     get idEmpleado(){
+       return this._idEmpleado;
+     }
 
     get sueldo(){
         return this._sueldo;
@@ -67,13 +67,34 @@ class Empleado extends Persona{
     }
 
     toString(){
-        return super.toString()+' '+this._sueldo;
+        return this._idEmpleado+' '+super.toString()+' '+this._sueldo;
     }
 }
 
-//clase empleado
-class Cliente{
+//clase Cliente
+class Cliente extends Persona{
+    static contadorCliente = 0;
+    constructor(nombre, apellido, edad, fechaRegistro){
+        super(nombre, apellido, edad)
+        this._idCliente = ++Cliente.contadorCliente;
+        this._fechRegistro = new Date();
+    }
 
+    get idCliente(){
+        return this._idCliente;
+    }
+
+    get fechaRegistro(){
+        return this._fechRegistro;
+    }
+
+    set fechaRegistro(fechaRegistro){
+        this._fechRegistro = fechaRegistro;
+    }
+
+    toString(){
+        return this._idCliente+' '+super.toString()+' '+this._fechRegistro;
+    }
 }
 
 let empleado1 = new Empleado('carlos', 'Mora', 23, 1500);
@@ -81,3 +102,10 @@ console.log(empleado1.toString());
 
 let empleado2 = new Empleado('Mario', 'Ozil', 33, 6000);
 console.log(empleado2.toString());
+
+//cliente
+let cliente1 = new Cliente('Ana', 'Perreira', 44 );
+console.log(cliente1.toString()) 
+
+let cliente2 = new Cliente('Manuel', 'Esparta', 25 );
+console.log(cliente2.toString())
