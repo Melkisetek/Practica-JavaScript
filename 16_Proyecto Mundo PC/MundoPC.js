5552d1a941a9d7113f3d89f3c80d7944e8002b18
melqui <elmenk2018@gmail.com>
2024-08-20 18:52:25 -0600

    actualizado

---
+++ 16_Proyecto Mundo PC/MundoPC.js
@@ -0,0 +1,144 @@
+class DispositivoEntrada{
+    constructor(tipoEntrada, marca){
+        this._tipoEntrada = tipoEntrada;
+        this._marca = marca;
+    }
+
+    get tipoEntrada(){
+        return this._tipoEntrada;
+    }
+
+    set tipoEntrada(tipoEntrada){
+        this._tipoEntrada = tipoEntrada;
+    }
+
+    get marca(){
+        return this._marca;
+    }
+
+    set marca(marca){
+        this._marca = marca;
+    }
+}
+
+class Raton extends DispositivoEntrada {
+    static contadorRatones = 0;
+    constructor(tipoEntrada, marca){
+        super(tipoEntrada, marca);
+        this._idRaton = ++Raton.contadorRatones;
+    }
+
+    get idRaton(){
+        return this._idRaton;
+    }
+     
+    toString(){
+        return `Raton: [idRaton: ${this._idRaton},
+                TipoEntrada: ${this._tipoEntrada},
+                marca: ${this._marca}]
+                 `;
+    }
+    
+}
+
+let raton1 = new Raton('USB', 'HP');
+console.log(raton1.toString());
+let raton2 = new Raton('USB', 'HP');
+raton2.marca = 'Lenovo'; 
+console.log(raton2.toString());
+
+
+class Teclado extends DispositivoEntrada{
+    static contadorTeclados = 0;
+    constructor(tipoEntrada, marca){
+        super(tipoEntrada, marca);
+        this._idTeclado = ++Teclado.contadorTeclados;
+    }
+
+    get idTeclado(){
+        return this._idTeclado;
+    }
+
+    toString(){
+        return `Teclado: [idTeclado: ${this._idTeclado},
+                tipoEntrada: ${this._tipoEntrada},
+                marca: ${this._marca}]`;
+    }
+
+}
+
+let teclado1 = new Teclado('Bluetooth', 'Dell');
+console.log(teclado1.toString());
+let teclado2 = new Teclado('USB', 'MSI');
+console.log(teclado2.toString());
+
+class Monitor{
+    static contadorMonitores = 0; 
+    constructor(marca, tamaño){
+        this._idMonitor = ++Monitor.contadorMonitores;
+        this._marca = marca;
+        this._tamaño = tamaño;
+    }
+
+    get idMonitor(){
+        return this._idMonitor;
+    }
+
+    get marca(){
+        return this._marca;
+    }
+
+    set marca(marca){
+        this._marca = marca;
+    }
+
+    get tamaño(){
+        return this._tamaño;
+    }
+
+    set tamaño(tamaño){
+        this._tamaño = tamaño;
+    }
+
+    toString(){
+        return `Monitor: [idMonitor: ${this._idMonitor},
+               Marca: ${this._marca},
+               Tamaño: ${this._tamaño}]`
+    }
+}
+
+let monitor1 = new Monitor('HP', 14);
+console.log(monitor1.toString());
+let monitor2 = new Monitor('Dell', '32');
+console.log(monitor2.toString());
+
+class Computadora{
+    static contadorComputadoras = 0;
+    //Utilizar concepto relación de agregación
+    constructor(nombre, monitor, teclado, raton){
+        this._idComputadora = ++Computadora.contadorComputadoras;
+        this._nombre = nombre;
+        this._monitor = monitor;
+        this._teclado = teclado;
+        this._raton = raton;
+        
+    }
+
+    get idComputadora(){
+        return this._idComputadora;
+    }
+
+    toString(){
+        //esto esta apuntando a un objeto de las clases se manda a llamar el método toString()
+        return `Computadora [ idComputadora: ${this._idComputadora}:
+                            ${this._nombre},
+                            \n${this._monitor},
+                            \n${this._teclado},
+                            \n${this._raton}]`
+    }
+}
+
+let computadora1 = new Computadora('MiEquipo', monitor1, teclado1, raton1);
+console.log(computadora1.toString());
+//forma 2 templey String
+//console.log(`${computadora1}`)
