dea1e29e2c1ecd8905b221ff68fffc0507c2027f
melqui <elmenk2018@gmail.com>
2024-09-06 18:56:51 -0600

    update

---
+++ 20_FuncionesFlecha/1_ArrowFunctions.js
@@ -0,0 +1,16 @@
+// función anónima lado derecho, asignado en una variable en el lado izquierdo.
+
+
+let miFuncion = function(){
+    console.log('Hola desde mi función');
+}
+
+miFuncion();
+
+// Función flecha
+let miFuncionFlecha = () =>{
+    console.log('Hola desde mi función flecha');
+}
+
+miFuncionFlecha();
+
